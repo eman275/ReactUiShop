d79e602a56df2017a28a53751b4bf0a2f773ee44
Em aN <44680040+eman275@users.noreply.github.com>
2021-10-16 19:47:54 +0200

    Shop Cart

---
+++ src/data.js
@@ -0,0 +1,25 @@
+
+//  import Shopping from '../img/shopping.png'
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cXFnLLV/3.pngg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
