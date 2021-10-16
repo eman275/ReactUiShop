e5ffb20b31252a869b8912d0566dcb2f37527c27
Em aN <44680040+eman275@users.noreply.github.com>
2021-10-16 20:08:06 +0200

    test

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -82,7 +82,7 @@ const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
   const handleClick = (direction) => {
     if (direction === "left") {
-      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 1);
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
     } else {
       setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
     }
